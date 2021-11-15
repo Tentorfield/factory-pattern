60ae038484981fee2a5b5a826c94e797b2da56d8
Tentorfield <hngo017@ucr.edu>
2021-11-15 12:58:22 -0800

    adding more starting files

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+#include <list>
+
+#include "Div.cpp"
+#include "Pow.cpp"
+#include "Sub.cpp"
+#include "Add.cpp"
+#include "Base.hpp"
+#include "Rand.cpp"
+#include "Op.cpp"
+//#include "Factory.cpp"
+
+int main(int argv, char** argc){
+		
+
+
+
+
+	return 0;
+}
