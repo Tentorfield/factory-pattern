24836bc3f0d8fb0a7c52be4f9135b6a26f1726dc
Harry64C <57604508+Harry64C@users.noreply.github.com>
2021-11-18 21:21:42 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -10,12 +10,23 @@
 #include "Rand.cpp"
 #include "Op.cpp"
 //#include "Factory.cpp"
+	
+using namespace std;
 
-int main(int argv, char** argc){
-		
+// "Client: only interacts with the AbstractFactory and AbstractProducts
+int main(int argv, char** argc) { // main executable
 
+    // char** argc; // takes input (expression) from user
+    // int argv; // char length of the expression
 
+    Base* expressionTree;
+    expressionTree = parse(argc, argv);
 
+    cout << "stringify: " << expressionTree->stringify() << "\n"
+    << "evaluate: " << expressionTree->evaluate() << endl;
 
-	return 0;
+    return 0;
 }
+
+
+
